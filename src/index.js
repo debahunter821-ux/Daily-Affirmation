8c3ede0040e5b1820ba72d19d94529a09a252441
debahunter821-ux <debahunter821@gmail.com>
2026-07-05 12:35:02 +0100

    removed console.log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayAffirmation(response) {
-  console.log("affirmation generated");
   new Typewriter("#affirmation-output", {
     strings: response.data.answer,
     autoStart: true,
@@ -19,10 +18,6 @@ function generateAffirmation(event) {
   let context =
     "User instructions: You are a helpful assistant that generates daily affirmations for users based on their input, using positive and uplifting language. The affirmations should be concise, clear, and easy to understand. They should also be relevant to the user's input and provide encouragement and motivation. Using basic html tags, format the output in a visually appealing way, with a focus on the affirmation itself. Avoid using complex language or technical terms, and instead use simple and relatable language that resonates with the user. The output should be in a single paragraph format, with no additional text or explanations. Sign it at the end with: by Debbie using AI. in <strong> element.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  console.log("Generating affirmation...");
-  console.log(`prompt: ${prompt}`);
-  console.log(`context: ${context}`);
 
   axios.get(apiUrl).then(displayAffirmation);
 
